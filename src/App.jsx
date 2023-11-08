7cde7c4b87a13668789a40be1354067f88d0722e
prof-awis <awinoemanuel2000@gmail.com>
2023-11-08 22:12:23 +0300

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-    <Navbar/>
-     <button className="btn btn-primary m-3">Button</button>
+      <Navbar />
+      <button className="btn btn-primary m-3">Button</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary mx-md-5">
+      <nav class="navbar navbar-expand-lg bg-body-info px-md-5">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
             Navbar
